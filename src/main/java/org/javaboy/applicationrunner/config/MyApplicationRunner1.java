f36a140557ce0a2b33c9bdf1e10d64cc99b27b1c
zhanghuipeng <1727149886@qq.com>
2020-04-07 15:57:16 +0800

    'first'

---
+++ src/main/java/org/javaboy/applicationrunner/config/MyApplicationRunner1.java
@@ -0,0 +1,30 @@
+package org.javaboy.applicationrunner.config;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Set;
+
+@Component
+@Order(99)
+public class MyApplicationRunner1 implements ApplicationRunner {
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        // 获取所有的参数
+        String[] sourceArgs = args.getSourceArgs();
+        System.out.println("sourceArgs:"+sourceArgs);
+        // 获取不是键值对的数据
+        List<String> nonOptionArgs = args.getNonOptionArgs();
+        System.out.println("nonOptionArgs:"+nonOptionArgs);
+        // 获取属于键值对数据的所有的key的set
+        Set<String> set = args.getOptionNames();
+        for (String key : set) {
+            // 根据key获取value
+            System.out.println((key+":"+args.getOptionValues(key)));
+        }
+
+    }
+}
